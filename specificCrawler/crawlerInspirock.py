4ebbd55131ae9e823ad62ab773c1d383c9eab103
Pragy Agarwal <agar.pragy@gmail.com>
2018-08-21 14:54:02 +0530

    Refactor crawlerInspirock

--- specificCrawler/crawlerInspirock.py
+++ specificCrawler/crawlerInspirock.py
@@ -11,23 +11,27 @@ from utilities import *
 # TODO: Make sure when aggregation is done, values are stripped of whitespace first
 
 urlToCityAndCountryMapping = {}
-def getStartingUrls(filePath = "Crawler/POI_Access_Data/inspirock_cities_access_url"):
-    urlsDetailFile = open(filePath, 'r')
 
-    citiesAndUrls = urlsDetailFile.readlines()
+
+def getStartingUrls(filePath = "Crawler/POI_Access_Data/inspirock_cities_access_url"):
+    with open(filePath, 'r') as f:
+        citiesAndUrls = f.readlines()
 
     startingUrls = []
     for cityAndUrl in citiesAndUrls:
-        [countryName, city, urlForcity] = cityAndUrl.split("\t")
-        urlForcity = urlForcity.strip()
-        print("processing:", urlForcity)
-        urlToCityAndCountryMapping[urlForcity] = {'city' : city, 'country' : countryName}
-        startingUrls.append(urlForcity)
-    urlsDetailFile.close()
+        countryName, city, cityURL = cityAndUrl.split('\t')
+        cityURL = cityURL.strip()
+
+        print("found:", cityURL)
+        urlToCityAndCountryMapping[cityURL] = {
+            'city': city,
+            'country': countryName}
+        startingUrls.append(cityURL)
+
     return startingUrls
 
 
-class CrawlerViator(scrapy.Spider):
+class CrawlerInspirock(scrapy.Spider):
     name = 'inspirock'
 
     start_urls = getStartingUrls()
@@ -42,7 +46,6 @@ class CrawlerViator(scrapy.Spider):
             meta['rank'] = attractionNumber
             yield response.follow(href, callback=self.parseAttractionsPage, meta = meta)
             attractionNumber += 1
-
 
     def parseAttractionsPage(self, response: scrapy.http.Response):
         breadcrumbs = response.css('div.where-in-world > span > a > span::text').extract()
@@ -66,58 +69,54 @@ class CrawlerViator(scrapy.Spider):
         if len(data) > 1:
             notes = data[1].css('::text').extract_first()
 
-
         ratingAndCountBox = response.css('div.rating > div.ins-rating')
+        # ratingBox = sideBox.css('p[itemprop="aggregateRating"]')
 
-        #ratingBox = sideBox.css('p[itemprop="aggregateRating"]')
         avgRating, ratingCount = None, None
         if ratingAndCountBox:
             bestRating = 5
             worstRating = 1
             givenRating = float(ratingAndCountBox.css('span::text').extract_first())
-            #ratingCount = int(ratingAndCountBox.css('div.countAndTotal > span[itemprop="ratingCount"]::text').extract_first())
+            # ratingCount = int(ratingAndCountBox.css('div.countAndTotal > span[itemprop="ratingCount"]::text').extract_first())
             avgRating = scaleRating(givenRating=givenRating, worstRating=worstRating, bestRating=bestRating)
-            
-
 
         rightBox = response.css('div.attraction-metadata > aside')
-        duration , address = None, None
+        duration, address = None, None
         if rightBox:
             duration = rightBox.css('div::text').extract()[1]
             address = rightBox.css('p::text').extract_first()
 
-        openingsHour , closingsHour = None, None
+        openingsHour, closingsHour = None, None
         openCloseBox = response.css('div.attraction-metadata > aside.opening-hours > div.clearfix > table > tr > td.time::text')
 
         if openCloseBox:
             self.log("times: "+response.url)
             hours = openCloseBox.extract()
-            openingsHour = ""
-            closingsHour = ""
+            openingsHour = ''
+            closingsHour = ''
             for hour in hours:
-                spliter = hour.split('-')
-                if(len(spliter) == 2):
-                    openingsHour += spliter[0].strip() + ","
-                    closingsHour += spliter[1].strip() + ","
+                splitter = hour.split('-')
+                if len(splitter) == 2:
+                    openingsHour += splitter[0].strip() + ','
+                    closingsHour += splitter[1].strip() + ','
                 else:
-                    openingsHour += spliter[0].strip() + ","
-                    closingsHour += spliter[0].strip() + ","
+                    openingsHour += splitter[0].strip() + ','
+                    closingsHour += splitter[0].strip() + ','
             openingsHour = openingsHour[:-2]
             closingsHour = closingsHour[:-2]
 
         pointListing = PointListing(crawler=self.name, sourceURL=response.url, crawlTimestamp=getCurrentTime(),
                                     countryName=countryName, cityName=cityName, pointName=pointName,
                                     description=description, notes=notes, address=address, 
-                                    openingHour = openingsHour, closingHour = closingsHour, recommendedNumHours = duration,
-                                    avgRating=avgRating, ratingCount=ratingCount, rank = response.meta['rank'])
+                                    openingHour=openingsHour, closingHour=closingsHour, recommendedNumHours=duration,
+                                    avgRating=avgRating, ratingCount=ratingCount, rank=response.meta['rank'])
 
         yield pointListing.jsonify()
 
+        pics = response.css('div.photos > div > ul > li > a > img::attr(src)')
 
-        ImageResource = pics = response.css('div.photos > div > ul > li > a > img::attr(src)')
-
-        if ImageResource:
-            pointImage = ImageResource.extract_first()
+        if pics:
+            pointImage = pics.extract_first()
             self.log("imageURL " + pointImage)
             #yield ImageResource(crawler=self.name, sourceURL=response.url, crawlTimestamp=getCurrentTime(),
                                 #countryName=countryName, cityName=cityName, pointName=pointName,
