1daca85e9f397a6a941157f01222c1dc1eff8567
Pragy Agarwal <agar.pragy@gmail.com>
2018-08-30 21:49:10 +0530

    Fix format time logic to consider opening closing hours.

--- src/aggregatorLogic.py
+++ src/aggregatorLogic.py
@@ -69,40 +69,53 @@ def getRecommendedNumHoursInHour(pointAggregated: PointAggregated):
         return result
 
 
-def formatTime(timeString: str):
+def formatTime(timeString: str, isOpen):
     """Converts time string to 24 hour format"""
+    timeString = timeString.lower()
+    if timeString == 'closed':
+        return '$'
+    if 'am' not in timeString and 'pm' not in timeString:
+        return None
 
-    if timeString == 'closed': return '$'
+    result = None
 
-    amFormatData = timeString.split('am')
-    if len(amFormatData) == 2:
-        hourAndMinutes = amFormatData[0].split(':')
-        hour = int(hourAndMinutes[0])
-        minutes = 0
-        if len(hourAndMinutes) == 2:  # think about 8 am
-            minutes = int(hourAndMinutes[1])
-        if hour == 12:
-            if minutes == 0:  # 12 am => 24
-                result = 24
-            else:   # 12:30 am => 0.5
+    if 'am' in timeString:
+        amFormatData = timeString.split('am')
+        if len(amFormatData) == 2:
+            hourAndMinutes = amFormatData[0].split(':')
+            hour = int(hourAndMinutes[0])
+            minutes = 0
+            if len(hourAndMinutes) == 2:  # think about 8 am
+                minutes = int(hourAndMinutes[1])
+            if hour == 12:
                 result = minutes / 60.0
-        else:  # 3:30 am => 3.5
-            result = hour + minutes / 60.0
-        return result
+            else:  # 3:30 am => 3.5
+                result = hour + minutes / 60.0
+    elif 'pm' in timeString:
+        pmFormatData = timeString.split('pm')
+        if len(pmFormatData) == 2:
+            hourAndMinutes = pmFormatData[0].split(':')
+            hour = int(hourAndMinutes[0])
+            minutes = 0
+            if len(hourAndMinutes) == 2: # think 9 pm
+                minutes = int(hourAndMinutes[1])
 
-    pmFormatData = timeString.split('pm')
-    if len(pmFormatData) == 2:
-        hourAndMinutes = pmFormatData[0].split(':')
-        hour = int(hourAndMinutes[0])
-        minutes = 0
-        if len(hourAndMinutes) == 2: # think 9 pm
-            minutes = int(hourAndMinutes[1])
+            if hour == 12:  # 12;30 pm => 12.5
+                result = hour + minutes / 60.0
+            else:  # 10:30 pm => 22.5
+                result = 12 + hour + minutes / 60.0
 
-        if hour == 12:  # 12;30 pm => 12.5
-            result = hour + minutes / 60.0
-        else:  # 10:30 pm => 22.5
-            result = 12 + hour + minutes / 60.0
-        return result
+    if isOpen and result > 22:
+        return 6
+
+    elif not isOpen and result < 8:
+        return 24
+
+    return result
+
+
+
+
 
 
 def processPointAggregated(pointAggregated: PointAggregated):
@@ -116,26 +129,26 @@ def processPointAggregated(pointAggregated: PointAggregated):
     if pointAggregated.openingHour is not None:
         openingHourDayWiseData = pointAggregated.openingHour.split(',')
         for openTime in openingHourDayWiseData:
-            processedOpenTime = formatTime(openTime.lower())
+            processedOpenTime = formatTime(openTime.lower(), isOpen=True)
             if processedOpenTime is None:                      # in case of openTime =  unavailable from tripExpert Data
-                processedOpenTime = formatTime(avgOpenTime.lower())
+                processedOpenTime = formatTime(avgOpenTime.lower(), isOpen=True)
             formattedOpeningHour += str(processedOpenTime) + ","
     else:
         openTime = avgOpenTime
         for i in range(7):
-            formattedOpeningHour += str(formatTime(openTime.lower())) + ","
+            formattedOpeningHour += str(formatTime(openTime.lower(), isOpen=True)) + ","
 
     if pointAggregated.closingHour is not None:
         closingHourDayWiseData = pointAggregated.closingHour.split(',')
         for closeTime in closingHourDayWiseData:
-            processedCloseTime = formatTime(closeTime)
+            processedCloseTime = formatTime(closeTime, isOpen=False)
             if processedCloseTime is None:
-                processedCloseTime = formatTime(avgCloseTime.lower())
+                processedCloseTime = formatTime(avgCloseTime.lower(), isOpen=False)
             formattedClosingHour += str(processedCloseTime) + ","
     else:
         closeTime = avgCloseTime
         for i in range(7):
-            formattedClosingHour += str(formatTime(closeTime.lower())) + ","
+            formattedClosingHour += str(formatTime(closeTime.lower(), isOpen=False)) + ","
 
     pointAggregated.openingHour = formattedOpeningHour[:-1]
     pointAggregated.closingHour = formattedClosingHour[:-1]
