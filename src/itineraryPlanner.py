a77fff186bcfd594aba10e67e735e3600e22fa38
deepak.ku <deepak.ku@mmmsadeepakku.internal.directi.com>
2018-09-02 17:35:59 +0530

    add mustnotvisitPoints parameter in getItinerary function to remove error in UI

--- src/itineraryPlanner.py
+++ src/itineraryPlanner.py
@@ -200,7 +200,7 @@ def possibleSequencesBWStartTimeAndEndPoint(listOfPoints, visitedPoints, current
     return possibleSequences
 
 
-def getDayItinerary(listOfPoints, mustVisitPoints, mustVisitPlaceEnterExitTime, dayStartTime,
+def getDayItinerary(listOfPoints, mustVisitPoints, mustVisitPlaceEnterExitTime,mustNotVisitPoints, dayStartTime,
                     dayEndTime, weekDay):
     possibleSequences = []
     visitedPoints = [False] * len(listOfPoints)
