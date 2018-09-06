79b02ed82af07ef32562964d097729e9856e4e77
deepak.ku <deepak.ku@mmmsadeepakku.internal.directi.com>
2018-09-06 13:40:30 +0530

    change tunable params for clustering

--- src/tunable.py
+++ src/tunable.py
@@ -215,10 +215,10 @@ fullConfig = {item: globals()[item] for item in dir() if not item.startswith("__
 distanceOverEstimatorFactor =  1.7
 weightOfMaxGscoreClusterSelection = 1
 weightOfAvgGscoreClusterSelection = 1
-weightOfNumPointsClusterSelection = 0.05
+weightOfNumPointsClusterSelection = 0.15
 
-weightOfDistancePointSelection = 3
-weightOfGscorePointSelection = 2
+weightOfDistancePointSelection = 1
+weightOfGscorePointSelection = 3
 
 outlierSelectionThreshold = 3
 distanceThresholdToRejectPoint = 100
